79ae4a6765096898f76ee5965c1e5ea75849a37d
Chris Beer <cabeer@stanford.edu>
2014-07-10 20:13:18 -0700

    Add a DOR most-recently-created item feed

---
+++ app/assets/javascripts/blocks/dor_feed_block.js
@@ -0,0 +1,13 @@
+SirTrevor.Blocks.Cal = (function() {
+  return Radiator.Block.extend({
+    title: function() { return "DOR Feed"; },
+    
+    icon_name: "DOR",
+    
+    type: "dor_feed",
+
+    metadata: function() {
+    },
+
+  });
+})();
