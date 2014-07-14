dab743a2e435e88d66aad8eaf6df3fef7846b780
Chris Beer <cabeer@stanford.edu>
2014-07-14 14:59:22 -0700

    add count and refresh interval to rss feeds

--- app/assets/javascripts/blocks/rss_feed_block.js
+++ app/assets/javascripts/blocks/rss_feed_block.js
@@ -9,6 +9,10 @@ SirTrevor.Blocks.RssFeed = (function() {
     metadata: function() {
       return this.text_field("url", "RSS/Atom URL");
     },
+    
+    sidebar: function() {
+        return this.text_field("count", "Count") + this.text_field("refresh", "Refresh interval (m)");
+    }
 
   });
 })();
